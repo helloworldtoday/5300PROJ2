3cc97030420d2c2478cb12164f5cd0537b9d663b
helloworldtoday <helloworldtoday@users.noreply.github.com>
2015-05-03 13:11:29 -0400

    Create SimpleMap.java

---
+++ SimpleMap.java
@@ -0,0 +1,44 @@
+import java.io.IOException;
+import java.util.*;
+
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.conf.*;
+import org.apache.hadoop.io.*;
+import org.apache.hadoop.mapreduce.*;
+import org.apache.hadoop.util.*;
+
+
+	public class SimpleMap extends Mapper<LongWritable, Text, Text, Text> {
+		// TODO map function
+		public void map(LongWritable key, Text value, Context context) throws IOException, InterruptedException {
+			// format: source, oldPR, list of destinations(maybe no outgoing)
+			String perLine = value.toString().trim();
+	  	  	String[] substring = perLine.split("\\s+");
+	  	  	
+	  	  	Text node = new Text(substring[0]);
+	  	  	double pageRankOld = Double.parseDouble(substring[1]);
+	  	  	int degrees = 0;
+	  	  	String outgoing = ""; 
+	  	  	
+	  	  	if (substring.length > 2) { // have outgoing
+	  	  		String[] outgoings = substring[2].split(",");
+	  	  		degrees = outgoings.length;
+	  	  		outgoing = substring[2];
+	  	  	}
+	  	  	
+	  	  	Text mykey = new Text(node);
+	  	  	Text value1 = new Text("pageRank;" + String.valueOf(pageRankOld) + ";" + outgoing);
+	  	  	context.write(mykey, value1);
+	  	  	
+	  	  	if (outgoing != "") {
+	  	  		double pageRankNew = Double.parseDouble(substring[1]);
+	  	  		Text value2 = new Text(String.valueOf(pageRankNew)); 
+	  	  		String[] outgoingAll = outgoing.split(",");
+	  	  		
+				for (String item: outgoingAll) {
+					mykey = new Text(item);
+					context.write(mykey, value2);
+				}
+	  	  	}			
+		}
+	}
