e33f96948fd62517e3368eb5a7d8e1867e2608e8
helloworldtoday <helloworldtoday@users.noreply.github.com>
2015-05-03 15:59:39 -0400

    Update SimpleMap.java

--- SimpleMap.java
+++ SimpleMap.java
@@ -9,36 +9,36 @@ import org.apache.hadoop.util.*;
 import org.apache.hadoop.io.Text;
 
 public class SimpleReduce extends Reducer<Text, Text, Text, Text> {
-		public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
+	public void reduce(Text key, Iterable<Text> values, Context context) throws IOException, InterruptedException {
 			
-			double damping = 0.85;
-			double pageRankAll = 0.0;
-			double pageRankNew = 0.0;
-			double pageRankOld = 0.0;
-			double residual = 0.0;
-			double randomJump = 1 - damping;
+		double damping = 0.85;
+		double pageRankAll = 0.0;
+		double pageRankNew = 0.0;
+		double pageRankOld = 0.0;
+		double residual = 0.0;
+		double randomJump = 1 - damping;
 			
-			String outgoing = "";
-			String output = "";
+		String outgoing = "";
+		String output = "";
 			
-			for (Text val : values) {
-				String[] array = val.toString().split(";");
-				if (array[0].equals("pageRank")) {
-					pageRankOld = Double.parseDouble(array[1]);
-					if (array.length > 2) {
-						outgoing = array[2];
-					} 
-				} else {
-					double pageRankPer = new Double(Double.parseDouble(array[0]));
-					pageRankAll += pageRankPer;
-				}
-			} 
-			pageRankNew = randomJump / SimplePR.numNode + (damping * pageRankAll);
-			residual = Math.abs(pageRankOld - pageRankNew) / pageRankNew;
-			long residualPer = (long) Math.ceil(residual * SimplePR.base);
-			context.getCounter(Counter.RESIDUAL).increment(residualPer);
+		for (Text val : values) {
+			String[] array = val.toString().split(";");
+			if (array[0].equals("pageRank")) {
+				pageRankOld = Double.parseDouble(array[1]);
+				if (array.length > 2) {
+					outgoing = array[2];
+				} 
+			} else {
+				double pageRankPer = new Double(Double.parseDouble(array[0]));
+				pageRankAll += pageRankPer;
+			}
+		} 
+		pageRankNew = randomJump / SimplePR.numNode + (damping * pageRankAll);
+		residual = Math.abs(pageRankOld - pageRankNew) / pageRankNew;
+		long residualPer = (long) Math.ceil(residual * SimplePR.base);
+		context.getCounter(Counter.RESIDUAL).increment(residualPer);
 			
-			Text value = new Text(pageRankNew + " " + outgoing);
-			context.write(key, value);
-		}
+		Text value = new Text(pageRankNew + " " + outgoing);
+		context.write(key, value);
+	}
 }
